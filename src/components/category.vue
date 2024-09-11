4b751fe027b79e0dec1436798d987b142d5ae71c
RAC_RSIT <rahat.ahmed@rootsoftit.com>
2024-09-11 15:20:02 +0600

    categories made dynamic

---
+++ src/components/category.vue
@@ -0,0 +1,25 @@
+<script setup>
+const props = defineProps({
+  modelValue: {
+    type: String,
+    default: "",
+  },
+  value: {
+    type: String,
+    default: "",
+  },
+});
+</script>
+
+<template>
+  <label>
+    <input
+      type="radio"
+      name="category"
+      :value="value"
+      @input="$emit('update:modelValue', value)"
+    />
+    <span :class="['bubble', value]"></span>
+    <div>{{ value }}</div>
+  </label>
+</template>
